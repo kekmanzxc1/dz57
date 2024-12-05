2a309b357f7fa5ca821b20d6267ac00529b1f146
kekmanzxc1 <stanislav61107@gmail.com>
2024-12-05 18:34:18 +0200

    Добавьте файлы проекта.

---
+++ dz57/dz57.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//Завдання 1 
+//int main() {
+//    string input;
+//    cout << "Enter a six-digit number: ";
+//    cin >> input;
+//
+//    if (input.length() != 6 || !isdigit(input[0])) {
+//        cout << "Error" << endl;
+//        return 1;
+//    }
+//
+//    int firstSum = 0, secondSum = 0;
+//    for (int i = 0; i < 3; ++i) {
+//        firstSum += input[i] - '0';
+//        secondSum += input[i + 3] - '0';
+//    }
+//
+//    if (firstSum == secondSum) {
+//        cout << "Number " << input << " is lucky" << endl;
+//    }
+//    else {
+//        cout << "Number " << input << " is not lucky" << endl;
+//    }
+//}
+//Завдання 2 
+int main() {
+    string input;
+    cout << "Enter a four-digit number: ";
+    cin >> input;
+
+    if (input.length() != 4 || !isdigit(input[0])) {
+        cout << "Error" << endl;
+        return 1;
+    }
+
+    swap(input[0], input[1]);
+
+    swap(input[2], input[3]);
+
+    cout << "The result after exchanging numbers: " << input << endl;
+}
